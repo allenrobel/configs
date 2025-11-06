786f237621f37db5476555b392706d22fb62243d
Allen Robel <arobel@cisco.com>
2025-11-05 18:26:54 -1000

    Rename and reorganize repository
    1. Rename from ‘configs’ to ‘ansibe-dcnm-setup’
    2. Update setup_repo.bash with new repo name
    2. Move all files out of subdirectory since this is such a small repo

--- setup_repo.bash
+++ setup_repo.bash
@@ -1,29 +1,32 @@
 #!/usr/bin/env bash
-# Define variables
+# Define script variables
 PYTHON_VERSION=3.11
 REPOS_HOME=$HOME/repos
 ANSIBLE_HOME=$REPOS_HOME/ansible
 ANSIBLE_COLLECTIONS_PATH=$ANSIBLE_HOME/collections
 REPO_DCNM=$ANSIBLE_COLLECTIONS_PATH/ansible_collections/cisco/dcnm
-REPO_CONFIGS=$REPOS_HOME/configs/dcnm
+REPO_SETUP=$REPOS_HOME/ansible-dcnm-setup
+
 # Create directory structure
 mkdir -p $ANSIBLE_COLLECTIONS_PATH/ansible_collections/cisco
 mkdir -p $ANSIBLE_COLLECTIONS_PATH/ansible_collections/ansible
+
 # Clone necessary repositories
 cd $ANSIBLE_COLLECTIONS_PATH/ansible_collections/ansible
 git clone https://github.com/ansible-collections/ansible.netcommon.git netcommon
 cd $ANSIBLE_COLLECTIONS_PATH/ansible_collections/cisco
 git clone https://github.com/CiscoDevNet/ansible-dcnm.git dcnm
-# Set up Python virtual environment and install dependencies in the dcnm repo
-cd $REPO_DCNM
+
+# Copy useful files into the dcnm repository
 mkdir -p $REPO_DCNM/env
-# cp $REPO_CONFIGS/requirements.txt $REPO_DCNM/requirements.txt
-# cp $REPO_CONFIGS/constraints.txt $REPO_DCNM/constraints.txt
-# cp $REPO_CONFIGS/uv.lock $REPO_DCNM/uv.lock
-cp $REPO_CONFIGS/pyproject.toml $REPO_DCNM/pyproject.toml
-cp $REPO_CONFIGS/pyrightconfig.json $REPO_DCNM/pyrightconfig.json
-cp $REPO_CONFIGS/env $REPO_DCNM/env/env
+cp $REPO_SETUP/pyproject.toml $REPO_DCNM/pyproject.toml
+cp $REPO_SETUP/pyrightconfig.json $REPO_DCNM/pyrightconfig.json
+cp $REPO_SETUP/env $REPO_DCNM/env/env
 cd $REPO_DCNM
+
+# Install uv and use to create virtual environment and install
+# dependencies required by the dcnm repository, including
+# Python 3.11, requests, ansible, pydantic, etc.
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 uv python install 3.11
