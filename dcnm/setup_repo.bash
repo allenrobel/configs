2e1bb1597dd1409dadf45cb66be15914a0d784f8
Allen Robel <arobel@cisco.com>
2025-11-05 16:36:28 -1000

    Remove setup_mac.bash and pip-related configs
    1. Modify setup_ubuntu.bash to work on both MacOS and Ubuntu
    2. Rename to setup_repo.bash
    3. Remove setup_mac.bash since we no longer need it
    4. Remove requirements.txt and constraints.txt since pyproject.toml covers these

--- dcnm/setup_repo.bash
+++ dcnm/setup_repo.bash
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 # Define variables
 PYTHON_VERSION=3.11
-ANSIBLE_HOME=$HOME/repos/ansible
+REPOS_HOME=$HOME/repos
+ANSIBLE_HOME=$REPOS_HOME/ansible
 ANSIBLE_COLLECTIONS_PATH=$ANSIBLE_HOME/collections
 REPO_DCNM=$ANSIBLE_COLLECTIONS_PATH/ansible_collections/cisco/dcnm
-REPO_CONFIGS=$HOME/repos/configs/dcnm
+REPO_CONFIGS=$REPOS_HOME/configs/dcnm
 # Create directory structure
-mkdir -p $ANSIBLE_HOME
-mkdir -p $ANSIBLE_COLLECTIONS_PATH
-mkdir -p $ANSIBLE_COLLECTIONS_PATH/ansible_collections
 mkdir -p $ANSIBLE_COLLECTIONS_PATH/ansible_collections/cisco
 mkdir -p $ANSIBLE_COLLECTIONS_PATH/ansible_collections/ansible
 # Clone necessary repositories
@@ -19,10 +17,10 @@ git clone https://github.com/CiscoDevNet/ansible-dcnm.git dcnm
 # Set up Python virtual environment and install dependencies in the dcnm repo
 cd $REPO_DCNM
 mkdir -p $REPO_DCNM/env
-cp $REPO_CONFIGS/requirements.txt $REPO_DCNM/requirements.txt
-cp $REPO_CONFIGS/constraints.txt $REPO_DCNM/constraints.txt
+# cp $REPO_CONFIGS/requirements.txt $REPO_DCNM/requirements.txt
+# cp $REPO_CONFIGS/constraints.txt $REPO_DCNM/constraints.txt
+# cp $REPO_CONFIGS/uv.lock $REPO_DCNM/uv.lock
 cp $REPO_CONFIGS/pyproject.toml $REPO_DCNM/pyproject.toml
-cp $REPO_CONFIGS/uv.lock $REPO_DCNM/uv.lock
 cp $REPO_CONFIGS/pyrightconfig.json $REPO_DCNM/pyrightconfig.json
 cp $REPO_CONFIGS/env $REPO_DCNM/env/env
 cd $REPO_DCNM
